60cbff65f475dd96d4f71456dd449acfdee2cecf
jonathan calderon <jaksvafa7@gmail.com>
2024-02-15 21:47:32 +0100

    el total puede ser reducido por el cliente

--- src/app/payout/payout.component.ts
+++ src/app/payout/payout.component.ts
@@ -10,6 +10,7 @@ import { RestService } from '../rest.service';
 export class PayoutComponent implements OnInit {
   // lista que hemos renderizado en la interfaz
   items_tobeSealed:any;
+  text_remove_button="Remove";
   suma:number=0;
   @ViewChild("sourcetag") myNameElem: ElementRef | undefined;
 
@@ -18,13 +19,11 @@ export class PayoutComponent implements OnInit {
   ngOnInit(): void {
     this.service.getClientPurchaseDataObservable().subscribe((value:any) => {
       this.items_tobeSealed=value['items'];
-      this.suma=this.suma+value.prize;
+      this.suma=value.prize;
     })
   }
 
-  makeSound(){
-    console.log(this.myNameElem);
-    let sound=this.myNameElem?.nativeElement;
-    sound.play();
+  removeFromCartByClient(index:number,prize:number){
+    this.service.removeProductFromCart(index,prize);
   }
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 export class RestService {
   product_items:object[]=[];
+  precioReducido=0;
   private ClientData={count:0,prize:0,items:this.product_items}
   private stringSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
   private ClientPurchaseData: BehaviorSubject<object> = new BehaviorSubject<object>(this.ClientData);
@@ -25,6 +26,14 @@ export class RestService {
     this.ClientData.prize=this.ClientData.prize+productPrize;
     console.log(this.ClientData)
     this.ClientPurchaseData.next(this.ClientData)
+  }
+
+  removeProductFromCart(indexToDelete:number,productPrize:number){
+    this.ClientData.items.splice(indexToDelete, 1);
+    this.ClientData.count=this.ClientData.items.length-1;
+    this.precioReducido=this.ClientData.prize-productPrize;
+    this.ClientData.prize=this.precioReducido;
+    this.ClientPurchaseData.next(this.ClientData);
   }
 
   getString() {
